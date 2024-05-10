2f5776c861e3e250ccc085a214a1ad92e854d478
HieuNguyen <hieunguyen31102004@gmail.com>
2024-05-10 17:17:52 +0700

    revert old change

--- NetworkScanner/obj/Debug/net8.0-windows/NetworkScanner.AssemblyInfo.cs
+++ NetworkScanner/obj/Debug/net8.0-windows/NetworkScanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetworkScanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7b49ff7285c7f9bad72a260ecfeb3110755e1f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38346b194fa6a9c5e226ebdda5965a465195c408")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetworkScanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetworkScanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
